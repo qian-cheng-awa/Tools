e4887b7920f7f9d3ae5523b7714d02cc5dd15426
_ <153122375+qian-cheng-awa@users.noreply.github.com>
2026-03-23 03:23:29 +0800

    Create Image.lua

---
+++ Image.lua
@@ -0,0 +1,19 @@
+local Module = {}
+
+Module.NewImage = function(Image)
+	if isfile and isfile(Image) then
+		Image = getcustomasset(Image)
+	end
+
+	local NewImage = game:GetService("AssetService"):CreateEditableImageAsync(Content.fromUri(Image))
+
+	return NewImage
+end
+
+Module.GetPixel = function(Image,X,Y)
+	local a = Image:ReadPixelsBuffer(Vector2.new(X,Y),Vector2.new(X,Y))
+	
+	return Color3.fromRGB(buffer.readu8(a,0),buffer.readu8(a,1),buffer.readu8(a,2)),buffer.readu8(a,3)/255 --Color,Transparency
+end
+
+return Module
